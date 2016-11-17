b462b848768eab8d0a558265e1c8a1eb85e8496a
Mimi <lifeistoogoodtobetrue@gmail.com>
2016-11-16 22:24:43 -0500

    U9 added code to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,45 +5,54 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne===cardTwo) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
+// if (cardOne===cardTwo) {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
+
+// if (cardOne===cardThree) {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
+
+// if (cardOne===cardFour) {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
+
+// if (cardTwo===cardThree) {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
+
+// if (cardTwo===cardFour) {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
+
+// if (cardThree===cardFour) {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again.")
+// }
+
+
+
+
+var createCards = function() {
+	var board = document.getElementById('game-board');
+
+ for (var i=0; i < 4; i++) {
+ 	var card = document.createElement('div');
+	card.className = 'card';
+	
+	board.appendChild(card);
+
 }
-
-if (cardOne===cardThree) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
-}
-
-if (cardOne===cardFour) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
-}
-
-if (cardTwo===cardThree) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
-}
-
-if (cardTwo===cardFour) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
-}
-
-if (cardThree===cardFour) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
-}
-
-
-
-
-// var board = document.getElementById('game-board');
-
-// for (var i; var)
+};
+createCards();
